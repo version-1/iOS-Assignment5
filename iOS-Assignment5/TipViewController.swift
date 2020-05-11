a071cc35107e7461a670ab7513a356746bf7fb43
version-1 <j.onodera26@gmail.com>
2020-05-11 07:10:12 -0700

    implement view

---
+++ iOS-Assignment5/TipViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TipViewController.swift
+//  iOS-Assignment5
+//
+//  Created by Administlator on 2020/05/11.
+//  Copyright © 2020 Administlator. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
