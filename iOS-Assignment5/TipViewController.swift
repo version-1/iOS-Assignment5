ced17ef56e025980b9932148eaf8587442a0fc4e
version-1 <j.onodera26@gmail.com>
2020-05-11 09:57:56 -0700

    add keyboard event

--- iOS-Assignment5/TipViewController.swift
+++ iOS-Assignment5/TipViewController.swift
@@ -15,11 +15,40 @@ class TipViewController: UIViewController {
     @IBOutlet var tipAmountLabel: UILabel!
     @IBOutlet var tipRateTextField: UITextField!
     @IBOutlet var tipRateSlider: UISlider!
+    
+    @IBOutlet var bottomViewConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         calculate()
         tipRateTextField.addTarget(self, action: #selector(rateTextFieldDichanged(textField:)), for: .editingChanged)
-        
+        NotificationCenter.default.addObserver(self,
+               selector: #selector(self.keyboardNotification(notification:)),
+               name: UIResponder.keyboardWillChangeFrameNotification,
+               object: nil)
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let endFrameY = endFrame?.origin.y ?? 0
+            let duration:TimeInterval = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIView.AnimationOptions = UIView.AnimationOptions(rawValue: animationCurveRaw)
+            if endFrameY >= UIScreen.main.bounds.size.height {
+                self.bottomViewConstraint?.constant = 0.0
+            } else {
+                self.bottomViewConstraint?.constant = endFrame?.size.height ?? 0.0
+            }
+            UIView.animate(withDuration: duration,
+                                       delay: TimeInterval(0),
+                                       options: animationCurve,
+                                       animations: { self.view.layoutIfNeeded() },
+                                       completion: nil)
+        }
     }
     
     func calculate() {
